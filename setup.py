243b3101ad1fe8e7ee2c9f3db40c188233f29db8
vyakhorev <Alexey.Vyakhorev@gmail.com>
2016-11-11 12:43:49 +0300

    Fixed setup.py
    When we need a readme and changes, we should add the files back and
    uncomment the lines in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
+# here = os.path.abspath(os.path.dirname(__file__))
+# with open(os.path.join(here, 'README.txt')) as f:
+#     README = f.read()
+# with open(os.path.join(here, 'CHANGES.txt')) as f:
+#     CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -32,7 +32,7 @@ tests_require = [
 setup(name='Thready',
       version='0.0',
       description='Thready',
-      long_description=README + '\n\n' + CHANGES,
+      #long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
